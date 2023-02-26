b1c2a6d0318a99bec0e78ec4a05139898e912f2a
daniar-17 <daniarnuramin1@gmail.com>
2023-02-26 21:28:38 +0700

    first commit

---
+++ app/Controllers/News.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class News extends BaseController
+{
+    public function index()
+    {
+        echo view('news');
+    }
+
+    //----------------------------------------------------
+
+}
